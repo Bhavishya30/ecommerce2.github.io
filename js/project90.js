0933599fbd5e8c1b42c132600d9b45a86e1c3384
Bhavishya30 <101133613+Bhavishya30@users.noreply.github.com>
2022-11-06 17:54:39 +0530

    Add files via upload

--- js/project90.js
+++ js/project90.js
@@ -10,56 +10,59 @@ let line3;
 let dropmenu;
 let content;
 let dropdown_icon;
-dropdown_icon=document.querySelectorAll(".dropdown-icon");
-dropmenu=document.getElementsByClassName("sidenav-item1");
-for( let i=0 ; i < dropmenu.length ; i++)
+let icon2;
+let icon3;
+icon2=document.querySelector(".icon-3");
+icon3=document.querySelector(".icon-3-content");
+icon2.addEventListener("click",xyz4);
+function xyz4()
 {
-       dropmenu[i].addEventListener("click",() =>
-       {
-       let result=dropmenu[i].classList.toggle("active6");
-       if(result)
-       {
-          dropdown_icon[i].classList.add("active8");
-       }
-       else
-       {
-         dropdown_icon[i].classList.remove("active8");
-       }
-       })
+  icon3.classList.toggle("active11");
 }
- hamburger=document.querySelector(".hamburger");
-line1=document.querySelector(".line-1");
-line2=document.querySelector(".line-2");
-line3=document.querySelector(".line-3")
-sidenav=document.querySelector(".sidenav");
-hamburger.addEventListener("click",xyz2);
-function xyz()
-{
-    let x;
-    x=searchbox.classList.toggle("active");
-    
-   
-    
+
+dropdown_icon = document.querySelectorAll(".dropdown-icon");
+dropmenu = document.getElementsByClassName("sidenav-item1");
+for (let i = 0; i < dropmenu.length; i++) {
+  dropmenu[i].addEventListener("click", () => {
+    let result = dropmenu[i].classList.toggle("active6");
+    if (result) {
+      dropdown_icon[i].classList.add("active8");
+    }
+    else {
+      dropdown_icon[i].classList.remove("active8");
+    }
+  })
 }
-function xyz2()
-{
-    sidenav.classList.toggle("active3");
-    line1.classList.toggle("active4");
-    line2.classList.toggle("active5");
-    line3.classList.toggle("active1");
+hamburger = document.querySelector(".hamburger");
+line1 = document.querySelector(".line-1");
+line2 = document.querySelector(".line-2");
+line3 = document.querySelector(".line-3")
+sidenav = document.querySelector(".sidenav");
+hamburger.addEventListener("click", xyz2);
+function xyz() {
+  let x;
+  x = searchbox.classList.toggle("active");
+
+
+
 }
-function xyz3()
-{
-  
+function xyz2() {
+  sidenav.classList.toggle("active3");
+  line1.classList.toggle("active4");
+  line2.classList.toggle("active5");
+  line3.classList.toggle("active1");
+}
+function xyz3() {
+
 }
 /*swiper js starts here*/
 var swiper = new Swiper(".home-slider", {
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
     // dynamicBullets: true,
-    loop:true,
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
   },
 });
 /*swiper js for second slider */
@@ -67,35 +70,35 @@ var swiper = new Swiper(".home-slider2", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-    grabCursor:true
+    grabCursor: true
   },
 });
 
 var swiper = new Swiper(".card-slider", {
   slidesPerView: 1,
   spaceBetween: 10,
-  loop:true,
-  autoplay:{
-    delay:2000,
+  loop: true,
+  autoplay: {
+    delay: 2000,
   }
   ,
   breakpoints: {
     550: {
       slidesPerView: 2,
       spaceBetween: 5,
-     
+
     },
     768: {
-      loop:true,
+      loop: true,
       slidesPerView: 2,
       spaceBetween: 20,
-      
+
     },
     1024: {
       slidesPerView: 3,
       spaceBetween: 10,
-     
- 
+
+
     },
   },
 });
@@ -124,23 +127,22 @@ var swiper = new Swiper(".arrivals-slider", {
     },
   },
 });
-const countdown =() =>
-{
+const countdown = () => {
   const countdate = new Date("Nov 29, 2022 00:00:00").getTime();
-  const now= new Date().getTime();
-  const gap = countdate -now;
+  const now = new Date().getTime();
+  const gap = countdate - now;
   const second = 1000;
-  const minute =second * 60;
-  const hour =minute * 60;
-  const day =hour * 24;
- const textday=Math.floor(gap /day); 
-const texthour= Math.floor((gap % day) / hour);
-const textminute =Math.floor((gap % hour) /minute);
-const textsecond =Math.floor((gap % minute) /second);
-document.querySelector(".day").innerText= textday;
-document.querySelector(".hour").innerText=texthour;
-document.querySelector(".min").innerText=textminute;
-document.querySelector(".sec").innerText=textsecond;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const textday = Math.floor(gap / day);
+  const texthour = Math.floor((gap % day) / hour);
+  const textminute = Math.floor((gap % hour) / minute);
+  const textsecond = Math.floor((gap % minute) / second);
+  document.querySelector(".day").innerText = textday;
+  document.querySelector(".hour").innerText = texthour;
+  document.querySelector(".min").innerText = textminute;
+  document.querySelector(".sec").innerText = textsecond;
 
 
 
@@ -171,6 +173,6 @@ var swiper = new Swiper(".seller-slider", {
       spaceBetween: 0,
     },
   },
-  
+
 
 });
